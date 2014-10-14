3827331e018dd5498ed57907c286f29896b784db
Rahul Horé <hore.rahul@gmail.com>
2014-10-14 16:00:13 -0400

    Adds radial edge bundling style & script

---
+++ scripts/radial_edge_bundling.js
@@ -0,0 +1,115 @@
+var diameter = 960,
+    radius = diameter / 2,
+    innerRadius = radius - 160;
+
+var cluster = d3.layout.cluster()
+                .size([360, innerRadius])
+                .sort(null)
+                .value(function(d) { return d.size; });
+
+var bundle = d3.layout.bundle();
+
+var line = d3.svg.line.radial()
+             .interpolate("bundle")
+             .tension(.85)
+             .radius(function(d) { return d.y; })
+             .angle(function(d) { return d.x / 180 * Math.PI; });
+
+var svg3 = d3.select("#radial_edge_bundling").append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .append("g")
+            .attr("transform", "translate(" + radius + "," + radius + ")");
+
+var link = svg3.append("g").selectAll(".link"),
+    node = svg3.append("g").selectAll(".node");
+
+d3.json("data/ancestry.json", function(error, classes) {
+  var nodes = cluster.nodes(klasses.root(classes)),
+      links = klasses.direct_ancestry(nodes);
+
+  link = link.data(bundle(links))
+             .enter().append("path")
+             .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
+             .attr("class", "link")
+             .attr("d", line);
+
+  node = node.data(nodes.filter(function(n) { return !n.children; }))
+             .enter().append("text")
+             .attr("class", "node")
+             .attr("dy", ".31em")
+             .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
+             .style("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+             .text(function(d) { return d.key; })
+             .on("mouseover", mouseovered)
+             .on("mouseout", mouseouted);
+});
+
+function mouseovered(d) {
+  node.each(function(n) { n.target = n.source = false; });
+
+  link.classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
+      .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+      .filter(function(l) { return l.target === d || l.source === d; })
+      .each(function() { this.parentNode.appendChild(this); });
+
+  node.classed("node--target", function(n) { return n.target; })
+      .classed("node--source", function(n) { return n.source; });
+}
+
+function mouseouted(d) {
+  link.classed("link--target", false)
+      .classed("link--source", false);
+
+  node.classed("node--target", false)
+      .classed("node--source", false);
+}
+
+d3.select(self.frameElement).style("height", diameter + "px");
+
+var klasses = {
+ 
+  // Lazily construct the package hierarchy from class names.
+  root: function(classes) {
+    var map = {};
+ 
+    function find(name, data) {
+      var node = map[name], i;
+      if (!node) {
+        node = map[name] = data || {name: name, children: []};
+        if (name.length) {
+          node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+          node.parent.children.push(node);
+          node.key = name.substring(i + 1);
+        }
+      }
+      return node;
+    }
+ 
+    classes.forEach(function(d) {
+      find(d.name, d);
+    });
+ 
+    return map[""];
+  },
+ 
+  // Return a list of direct_ancestry for the given array of nodes.
+  direct_ancestry: function(nodes) {
+    var map = {},
+        direct_ancestry = [];
+ 
+    // Compute a map from name to node.
+    nodes.forEach(function(d) {
+      map[d.name] = d;
+    });
+ 
+    // For each class, construct a link from the source to target node.
+    nodes.forEach(function(d) {
+      if (d.direct_ancestry) d.direct_ancestry.forEach(function(i) {
+        direct_ancestry.push({source: map[d.name], target: map[i]});
+      });
+    });
+ 
+    return direct_ancestry;
+  }
+};
